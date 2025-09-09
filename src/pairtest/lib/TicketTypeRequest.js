2c049f4cca0aedae3374bfc6bd71ee0775d8e4f0
Cephaz6 <cephasblog@gmail.com>
2025-09-09 22:54:28 +0100

    More template files

---
+++ src/pairtest/lib/TicketTypeRequest.js
@@ -0,0 +1,37 @@
+/**
+ * Immutable Object for ticket type requests.
+ * Enhanced from template with additional validation.
+ */
+export default class TicketTypeRequest {
+  #type;
+  #noOfTickets;
+
+  constructor(type, noOfTickets) {
+    if (!this.#Type.includes(type)) {
+      throw new TypeError(
+        `type must be ${this.#Type
+          .slice(0, -1)
+          .join(", ")}, or ${this.#Type.slice(-1)}`
+      );
+    }
+    if (!Number.isInteger(noOfTickets)) {
+      throw new TypeError("noOfTickets must be an integer");
+    }
+    if (noOfTickets < 0) {
+      throw new TypeError("noOfTickets must be a positive integer");
+    }
+
+    this.#type = type;
+    this.#noOfTickets = noOfTickets;
+  }
+
+  getNoOfTickets() {
+    return this.#noOfTickets;
+  }
+
+  getTicketType() {
+    return this.#type;
+  }
+
+  #Type = ["ADULT", "CHILD", "INFANT"];
+}
